20b43b84623b7b59280bccc9a0c98c50fa51feda
Bomanaaberti <142838385+Bomanaabertilla@users.noreply.github.com>
2023-12-11 02:20:14 +0000

    Add files via upload

---
+++ myclass.java
@@ -0,0 +1,31 @@
+MyClass.java
+
+public class MyClass{
+
+ public static void main(String []args){
+
+   System.out.println("Hello World");
+
+// This is a comment
+System.out.println("Hello World");
+
+/* The code below will print the words Hello World to the screen, and it is amazing*/
+System.out.println("Hello World");
+
+/ This is how to declare a string
+
+String name = "Bertilla";
+System.out.println(name);
+
+/This is how to declare an integer
+
+int myNum = 15;
+System.out.println(myNum);
+
+ 
+
+  }
+
+}
+
+
